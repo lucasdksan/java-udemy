fa3e2233fec945a33b5bbff77476fd5c6c8814eb
lucasdksan <lucas.leoncio.silva@gmail.com>
2024-11-19 18:36:04 -0300

    Desafio do curso

---
+++ faculty/src/Course/Course.java
@@ -0,0 +1,7 @@
+package Course;
+
+public class Course {
+    public static void main(String[] args) {
+        System.out.println("Curso da Faculdade!");
+    }
+}
---
+++ faculty/src/Discipline/Discipline.java
@@ -0,0 +1,7 @@
+package Discipline;
+
+public class Discipline {
+    public static void main(String[] args) {
+        System.out.println("Disciplina da Faculdade!");
+    }
+}
